1989cffc3fbcdea49a9bbe1bd707e5172e3f877a
Sana <j.lepretre@gmail.com>
2025-03-19 14:54:14 +0100

    Setup

---
+++ LammpsThermo.py
@@ -0,0 +1,11 @@
+def graph():
+    # produce a nice png with convenient legend
+    # should be used as "graph(enthalpy, temperature)" for example in order to graph the enthalpy as a function of the temperature
+    # add option to also infer some modelisation of the data
+    pass
+
+
+def graph_heat_capacity():
+    # higher level function?
+    # needs to take vibrations into account
+    pass
